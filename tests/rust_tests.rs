1852d86a5335ce146e534e9ce090bfe8b0e39768
David Bucur <dbucur22@gmail.com>
2022-11-29 18:44:56 +0200

    added mint token tests #6

--- tests/rust_tests.rs
+++ tests/rust_tests.rs
@@ -4,6 +4,7 @@ use datanftmint::nft_mint_utils::*;
 use datanftmint::requirements::RequirementsModule;
 use datanftmint::storage::*;
 use datanftmint::*;
+use elrond_wasm::contract_base::ContractBase;
 use elrond_wasm::elrond_codec::multi_encode_iter_or_handle_err;
 use elrond_wasm::hex_literal;
 use elrond_wasm::types::{ManagedAddress, MultiValueEncoded};
@@ -26,8 +27,6 @@ pub const SFT_TICKER: &[u8] = b"DATANFTFT-1a2b3c";
 pub const SFT_NAME: &[u8] = b"DATA NFT-FT";
 pub const DATA_MARCHAL: &[u8] = b"DATA-MARCHAL-ENCRYPTED";
 pub const DATA_STREAM: &[u8] = b"DATA-STREAM-ECRYPTED";
-pub const DATA_MARCHAL_STREAM_SHA256: [u8; 32] =
-    hex_literal::hex!("8c195f3b03f86fe51521c0ec6d353b58b635cf76362ed4731d4b90797b622865");
 pub const MEDIA_URI: &[u8] = b"https://ipfs.io/ipfs/123456abcdef/metadata.json";
 pub const USER_NFT_NAME: &[u8] = b"USER-NFT-NAME";
 pub const MINT_TIME_LIMIT: u64 = 15;
@@ -300,14 +299,14 @@ fn nft_mint_utils_test() {
 
     b_wrapper
         .execute_query(&setup.contract_wrapper, |sc| {
+            let data_buffer = managed_buffer!(&[DATA_MARCHAL, DATA_STREAM].concat());
+            let data_hash = sc.crypto().sha256(data_buffer).as_managed_buffer().clone();
             assert_eq!(
-                (sc.crate_hash_buffer(
+                sc.crate_hash_buffer(
                     &managed_buffer!(DATA_MARCHAL),
                     &managed_buffer!(DATA_STREAM)
-                )
-                .to_boxed_bytes()
-                .into_vec()),
-                DATA_MARCHAL_STREAM_SHA256
+                ),
+                data_hash
             );
         })
         .assert_ok();
@@ -418,17 +417,80 @@ fn requirements_test() {
         })
         .assert_ok();
 
+    // // ????
+    // b_wrapper
+    //     .execute_query(&setup.contract_wrapper, |sc| {
+    //         sc.require_is_privileged(&managed_address!(first_user_address))
+    //     })
+    //     .assert_user_error("Address is not privileged");
+
     b_wrapper
         .execute_query(&setup.contract_wrapper, |sc| {
-            sc.require_is_privileged(&managed_address!(first_user_address))
+            sc.require_value_is_positive(&managed_biguint!(0u64));
         })
-        .assert_user_error("Address is not privileged");
+        .assert_error(4, "Value must be positive");
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_sft_is_valid(&managed_biguint!(90000u64), &managed_biguint!(2u64));
+        })
+        .assert_error(4, "Royalties are bigger than max royalties");
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_sft_is_valid(&managed_biguint!(u8::MIN), &managed_biguint!(2u64));
+        })
+        .assert_ok();
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_sft_is_valid(&managed_biguint!(u8::MIN), &managed_biguint!(23u64));
+        })
+        .assert_error(4, "Max supply exceeded");
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_sft_is_valid(&managed_biguint!(u8::MIN), &managed_biguint!(0u64));
+        })
+        .assert_error(4, "Supply must be positive");
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_minting_is_allowed(&managed_address!(first_user_address), 0u64);
+        })
+        .assert_error(4, "You need to wait more time before minting again");
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_minting_is_allowed(&managed_address!(first_user_address), 15u64);
+        })
+        .assert_error(4, "You are not whitelisted");
+
+    b_wrapper
+        .execute_tx(
+            &owner_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                let mut args = MultiValueEncoded::new();
+                args.push(managed_address!(&first_user_address));
+                sc.set_whitelist_spots(args);
+            },
+        )
+        .assert_ok();
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.require_minting_is_allowed(&managed_address!(first_user_address), 15u64);
+        })
+        .assert_ok();
 }
 
 #[test] // Tests whether minting works correctly.
 fn mint_nft_ft_test() {
     let mut setup = setup_contract(datanftmint::contract_obj);
     let b_wrapper = &mut setup.blockchain_wrapper;
+    let owner_address = &setup.owner_address;
     let first_user_address = &setup.first_user_address;
 
     b_wrapper
@@ -451,4 +513,215 @@ fn mint_nft_ft_test() {
             },
         )
         .assert_error(4, "Minting is not ready");
+
+    b_wrapper
+        .execute_tx(
+            &owner_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                sc.set_is_paused(false);
+            },
+        )
+        .assert_ok();
+
+    b_wrapper
+        .execute_tx(
+            &owner_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(5u64 * 10u64.pow(16u32)),
+            |sc| sc.token_id().set_token_id(managed_token_id!(SFT_TICKER)),
+        )
+        .assert_ok();
+
+    b_wrapper
+        .execute_tx(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(20),
+                    managed_biguint!(0),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_error(4, "Supply must be positive");
+
+    b_wrapper
+        .execute_tx(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(90000),
+                    managed_biguint!(1),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_error(4, "Royalties are bigger than max royalties");
+
+    b_wrapper
+        .execute_tx(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(20),
+                    managed_biguint!(5),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_error(4, "You are not whitelisted");
+
+    b_wrapper
+        .execute_tx(
+            &owner_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                sc.set_mint_time_limit(15u64);
+            },
+        )
+        .assert_ok();
+
+    b_wrapper
+        .execute_tx(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(20),
+                    managed_biguint!(5),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_error(4, "You need to wait more time before minting again");
+
+    b_wrapper
+        .execute_tx(
+            &owner_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                sc.set_mint_time_limit(0u64);
+            },
+        )
+        .assert_ok();
+
+    b_wrapper
+        .execute_tx(
+            &owner_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0),
+            |sc| {
+                let mut args = MultiValueEncoded::new();
+                args.push(managed_address!(&first_user_address));
+                sc.set_whitelist_spots(args);
+            },
+        )
+        .assert_ok();
+
+    b_wrapper
+        .execute_tx(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(0u64),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(20),
+                    managed_biguint!(5),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_error(4, "Value must be positive");
+
+    b_wrapper
+        .execute_tx(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &rust_biguint!(2u64),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(20),
+                    managed_biguint!(5),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_error(4, "Wrong amount of payment sent");
+
+    b_wrapper
+        .execute_query(&setup.contract_wrapper, |sc| {
+            sc.anti_spam_tax(&managed_token_id_wrapped!(TOKEN_ID))
+                .set(managed_biguint!(2));
+        })
+        .assert_ok();
+
+    b_wrapper
+        .execute_esdt_transfer(
+            &first_user_address,
+            &setup.contract_wrapper,
+            &TOKEN_ID,
+            0,
+            &rust_biguint!(2),
+            |sc| {
+                sc.mint_token(
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(MEDIA_URI),
+                    managed_buffer!(DATA_MARCHAL),
+                    managed_buffer!(DATA_STREAM),
+                    managed_buffer!(DATA_STREAM),
+                    managed_biguint!(20),
+                    managed_biguint!(5),
+                    managed_buffer!(USER_NFT_NAME),
+                    managed_buffer!(USER_NFT_NAME),
+                );
+            },
+        )
+        .assert_ok();
 }
