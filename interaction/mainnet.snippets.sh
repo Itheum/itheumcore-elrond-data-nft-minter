beebc905244d674984c4dc4a2033fdcdd12cd944
Mark Paul <paul.mark@gmail.com>
2023-12-13 16:26:57 +1100

    added upgrade method to devnet and mainnet snippets

--- interaction/mainnet.snippets.sh
+++ interaction/mainnet.snippets.sh
@@ -7,6 +7,10 @@ DEPLOY_TRANSACTION=$(mxpy data load --key=deployTransaction-mainnet)
 TOKEN="ITHEUM-df6f26"
 TOKEN_HEX="0x$(echo -n ${TOKEN} | xxd -p -u | tr -d '\n')"
 
+# to deploy from last reprodubible build, we need to change or vice versa
+# --bytecode output/datanftmint.wasm \
+# to 
+# --bytecode output-docker/datanftmint/datanftmint.wasm 
 deployLedgerMainnet(){
     mxpy --verbose contract deploy \
     --bytecode output-docker/datanftmint/datanftmint.wasm \
@@ -29,10 +33,33 @@ deployLedgerMainnet(){
     mxpy data store --key=deployTransaction-mainnet --value=${TRANSACTION}
 }
 
+# any change to code or property requires a full upgrade 
+# always check if you are deploy via a reprodubible build and that the code hash is the same before and after upgrade (that is if you are only changing props and not code.. for code, the RB will be different)
+# if only changing props, you can't just "append" new props. you have to add the old ones again and then add a new prop you need. i.e. it's not append, it's a whole reset
+# for upgrade, --outfile deployOutput is not needed
+# in below code example we added --metadata-payable to add PAYABLE to the prop of the SC and removed --metadata-not-readable to make it READABLE
+upgrade(){
+    mxpy --verbose contract upgrade ${ADDRESS} \
+    --bytecode output-docker/datanftmint/datanftmint.wasm \
+    --metadata-payable-by-sc \
+    --metadata-payable \
+    --proxy ${PROXY} \
+    --chain ${CHAIN_ID} \
+    --gas-limit 150000000 \
+    --recall-nonce \
+    --ledger \
+    --ledger-address-index 0 \
+    --send || return
+}
+
 # if you interact without calling deploy(), then you need to 1st run this to restore the vars from data
 restoreDeployDataLedgerMainnet() {
   TRANSACTION=$(mxpy data parse --file="./interaction/deploy-mainnet.interaction.json" --expression="data['emittedTransactionHash']")
   ADDRESS=$(mxpy data parse --file="./interaction/deploy-mainnet.interaction.json" --expression="data['contractAddress']")
+
+  # after we upgraded to mxpy 8.1.2, mxpy data parse seems to load the ADDRESS correctly but it breaks when used below with a weird "Bad address" error
+  # so, we just hardcode the ADDRESS here. Just make sure you use the "data['contractAddress'] from the latest deploy-devnet.interaction.json file
+  ADDRESS="erd1qqqqqqqqqqqqqpgqmuzgkurn657afd3r2aldqy2snsknwvrhc77q3lj8l6"
 }
 
 initializeContractMainnet(){
